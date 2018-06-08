1e5baa34ce9588d5980c30d3983161849b4c79c5
ZhaoYu <zhaoyu.johnny@gmail.com>
2018-06-09 02:06:36 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const electron = require("electron"),
+  app = electron.app,
+  BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 640,
+    height: 480
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  //mainWindow.webContents.openDevTools();
+  mainWindow.on('close', () => {
+    mainWindow.webContents.send('stop-server');
+  });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+app.on("browser-window-created", function (e, window) {
+  window.setMenu(null);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
