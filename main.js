c73fc0560fd9e313d3cfe740849dc7dbbd8f223e
datudou <wangqianyi.cs@gmail.com>
2018-06-10 16:10:57 +0900

    add paste and copy function

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const electron = require("electron"),
   app = electron.app,
   BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow;
+
+
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -18,7 +21,68 @@ function createWindow() {
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
+  createMenu();
 }
+
+function createMenu() {
+  // Create the Application's main menu
+  var template = [{
+    label: "Application",
+    submenu: [{
+        label: "About Application",
+        selector: "orderFrontStandardAboutPanel:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Quit",
+        accelerator: "Command+Q",
+        click: function () {
+          app.quit();
+        }
+      }
+    ]
+  }, {
+    label: "Edit",
+    submenu: [{
+        label: "Undo",
+        accelerator: "CmdOrCtrl+Z",
+        selector: "undo:"
+      },
+      {
+        label: "Redo",
+        accelerator: "Shift+CmdOrCtrl+Z",
+        selector: "redo:"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Cut",
+        accelerator: "CmdOrCtrl+X",
+        selector: "cut:"
+      },
+      {
+        label: "Copy",
+        accelerator: "CmdOrCtrl+C",
+        selector: "copy:"
+      },
+      {
+        label: "Paste",
+        accelerator: "CmdOrCtrl+V",
+        selector: "paste:"
+      },
+      {
+        label: "Select All",
+        accelerator: "CmdOrCtrl+A",
+        selector: "selectAll:"
+      }
+    ]
+  }];
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+}
+
 
 app.on("ready", createWindow);
 app.on("browser-window-created", function (e, window) {
